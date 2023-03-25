060549c3f1799f7af6cb9dcd44c609f43bada732
alba.ramos <alba.ramos@ignisenergia.es>
2023-03-25 18:11:39 +0100

    First prototype

--- matcher/matcher.py
+++ matcher/matcher.py
@@ -1,15 +1,46 @@
 from interfaces.public import AbstractMatcher
+from person.person import Person
+
+
+class PersonAlreadyExists(Exception):
+    ...
 
 
 class MongoMatcher(AbstractMatcher):
-    def __init__(self, settings, database):
-        self.settings = settings
-        self.database = database
+    def __init__(self, client):
+        self.client = client
+        self.database = self.client['family_tree_matcher']
+        self.collection = self.database['people']
 
-    def match(self, person: dict) -> dict:
-        collection = self.database[
-            self.settings.FAMILY_TREE_DATABASE][
-            self.settings.PEOPLE_COLLECTION]
-        print(collection.find_one({}))
-        #print("AH")
-        return {}
+    def _check_person_exists(self, person: Person):
+        # TODO modify this to check ancestors to determine if it's really the same person
+        if self.collection.count_documents({
+            'name': person.name,
+            'surname': person.surname
+        }) > 0:
+            raise PersonAlreadyExists("A person with the same name is already registered")
+
+    def _get_person_siblings(self, person: Person) -> list:
+        # TODO check ancestors too
+        return [x for x in self.collection.find({
+            'surname': {
+                '$regex': ' '.join(['(' + element + ')' for element in person.surname.split(' ')])
+            }
+        })]
+
+    def match(self, person: Person) -> list:
+        match = []
+        if person.name and person.surname:
+            self._check_person_exists(person)
+            brothers = self._get_person_siblings(person)
+
+            # TODO match other type of relationships
+
+            if len(brothers) > 0:
+                match = brothers
+
+        return match
+
+    # TODO a factory pattern to create people that don't exist in the DB?
+    # TODO an observer pattern to update people when a family member is created
+
--- tests/matcher/test_matcher.py
+++ tests/matcher/test_matcher.py
@@ -1,4 +1,5 @@
-from matcher.matcher import MongoMatcher
+from matcher.matcher import MongoMatcher, PersonAlreadyExists
+from person.person import Person
 import unittest
 import json
 import os
@@ -7,20 +8,46 @@ from mongomock import MongoClient
 
 
 class TestMatcher(unittest.TestCase):
-    def setUp(self):
-        self.fixture_db = test.CLOUD_MONGO_CLIENT
-        self.sut = MongoMatcher(test, self.fixture_db)
+    fixture_db = test.CLOUD_MONGO_CLIENT
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sut = MongoMatcher(cls.fixture_db)
 
         with open(os.path.join(os.getcwd(), 'fixtures/fixture_documents.json'), 'r') as f:
-            self.fixture_docs = json.loads(f.read())
+            fixture_docs = json.loads(f.read())
 
-        for pk in self.fixture_docs.keys():
-            self.fixture_db[
+        for pk in fixture_docs.keys():
+            cls.fixture_db[
                 test.FAMILY_TREE_DATABASE][
-                test.PEOPLE_COLLECTION].insert_one(self.fixture_docs[pk])
+                test.PEOPLE_COLLECTION].insert_one(fixture_docs[pk])
 
     def test_mocked_db(self):
         self.assertIsInstance(self.fixture_db, MongoClient)
 
+    def test_malformed_person(self):
+        self.assertEqual([], self.sut.match(Person(None, None)))
+        self.assertEqual([], self.sut.match(Person('alba', None)))
+
+    def test_already_existing_person(self):
+        fixture_person = Person('alba', 'ramos pedroviejo')
+        self.assertRaises(PersonAlreadyExists, self.sut.match, fixture_person)
+
     def test_match(self):
-        self.sut.match({})
+        # Match one brother
+        fixture_person = Person('julio', 'ramos pedroviejo')
+        obtained = self.sut.match(fixture_person)
+        self.assertEqual(len(obtained), 1)
+        self.assertEqual(obtained[0]['name'], 'alba')
+        self.assertEqual(obtained[0]['surname'], 'ramos pedroviejo')
+
+        # Match several brothers
+
+        # Match a parent
+
+    def test_no_match(self):
+        fixture_person = Person('julio', 'marquez dominguez')
+        self.assertEqual(self.sut.match(fixture_person), [])
+
+
+
